08fea960b2c8d943c7cc9800734af2a826ac2880
Pablo Cabrera <binchmod777@gmail.com>
2016-12-31 16:12:19 -0300

    Cliente javascript cache de tiles y refresh de vista

--- src/AreaRenderer.js
+++ src/AreaRenderer.js
@@ -4,6 +4,7 @@ AreaRenderer = function (area) {
 	this.viewSize = [64, 64];
 	this.renderSize = [300, 300];
 	this.drawSvg = null;
+	this.tileCache = [];
 }
 
 /* Crear elemento SVG y poner en container */
@@ -24,6 +25,7 @@ AreaRenderer.prototype.adjust = function () {
 		+ this.viewSize[0] + " "
 		+ this.viewSize[1]
 	);
+	this.refresh();
 }
 
 /* Dibujar */
@@ -39,10 +41,86 @@ AreaRenderer.prototype.draw = function () {
 		for (var y = start_y; y < end_y; y++) {
 			var surface = this.area.getSurfaceAt(x, y);
 			var tile = TileFactory.getTile(surface);
-			/* FALTA CACHEAR PARA PODER ELIMINAR */
-			tile.setAttributeNS (null, "x", x);
-			tile.setAttributeNS (null, "y", y);
-			this.drawSvg.appendChild(tile);
+			this.putTile (tile, x, y);
+		}
+	}
+}
+
+/* Dibuja un tile y lo guarda en cache*/
+AreaRenderer.prototype.putTile = function (tile, x, y) {
+	tile.setAttributeNS (null, "x", x);
+	tile.setAttributeNS (null, "y", y);
+	this.drawSvg.appendChild(tile);
+	this.cacheTile (tile, x, y);
+}
+
+/* Guarda un tile en cache */
+AreaRenderer.prototype.cacheTile = function (tile, x, y) {
+	if(this.tileCache[x] == undefined) {
+		this.tileCache[x] = [];
+	}
+	this.tileCache[x][y] = tile;
+}
+
+/* Actualizar imagen */
+AreaRenderer.prototype.refresh = function () {
+	this.deleteTilesOutsideView();
+	this.drawRemainingTiles();
+}
+
+/* Eliminar los tiles de la cache  que estan fuera del area dibujable */
+AreaRenderer.prototype.deleteTilesOutsideView = function () {
+	var column = Math.floor(this.viewOrigin[0]);
+	var limitColumn = column + this.viewSize[0] + 1;
+	
+	var row = Math.floor (this.viewOrigin[1]);
+	var limitRow = row + this.viewSize[1] + 1;
+
+	this.deleteElementsOutsideRange(this.tileCache, column, limitColumn);
+	this.deleteCacheRowsOutside(row, limitRow);
+}
+
+/* Eliminar los valores de un vector que estan fuera de un rango */
+AreaRenderer.prototype.deleteElementsOutsideRange = function (vector, inferior, superior) {
+	var index;
+	for (index = inferior-1; vector[index] != undefined; index--){
+		delete vector[index];
+	}
+
+	for (index = superior+1; vector[index] != undefined; index++){
+		delete vector[index];
+	}
+}
+
+/* Eliminar filas de cache fuera de un rango */
+AreaRenderer.prototype.deleteCacheRowsOutside = function (inferior, superior) {
+	var self = this;
+	this.tileCache.forEach(function (vector) {
+		self.deleteElementsOutsideRange (vector, inferior, superior);
+	});
+}
+
+
+/* Dibujar los tiles que faltan para completar la zona de vision */
+AreaRenderer.prototype.drawRemainingTiles = function () {
+	var start_x = Math.floor(this.viewOrigin[0]);
+	var start_y = Math.floor(this.viewOrigin[1])
+	
+	var limit_x = Math.ceil(this.viewOrigin[0] + this.viewSize[0]);
+	var limit_y = Math.ceil(this.viewOrigin[1] + this.viewSize[1]);
+
+
+	
+	for (var x = start_x; x < limit_x; x++) {
+		for (var y = start_y;y < limit_y; y++) {
+			if (this.tileCache[x] == undefined) {
+				this.tileCache[x] = [];
+			}
+			if (this.tileCache[x][y] == undefined) {
+				var tile = TileFactory.getTile (this.area.getSurfaceAt(x, y));
+				this.putTile(tile, x, y);
+			}
+
 		}
 	}
 }
@@ -55,7 +133,7 @@ AreaRenderer.prototype.goTo = function (destination) {
 
 /* Establecer un nivel de zoom */
 AreaRenderer.prototype.setZoomLevel = function (zoomLevel) {
-	this.setViewSize ([10/zoomLevel], [10/zoomLevel]);
+	this.setViewSize ([10/zoomLevel, 10/zoomLevel]);
 	this.adjust();
 }
 
