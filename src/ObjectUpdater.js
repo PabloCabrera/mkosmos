1897634c22f5b5f57f53d63bd8031eaaff9d5290
Pablo Cabrera <binchmod777@gmail.com>
2017-02-22 15:10:37 -0300

    Colision entre dos objetos

--- src/ObjectUpdater.js
+++ src/ObjectUpdater.js
@@ -4,6 +4,7 @@ ObjectUpdater = function (object) {
 	this.sendMsgInterval = 1000;
 	this.lastSendMsg = Date.now ();
 	this.area = null;
+	this.collisionChecks=[];
 }
 
 ObjectUpdater.prototype.step = function (now) {
@@ -12,8 +13,32 @@ ObjectUpdater.prototype.step = function (now) {
 	this.object.y += this.object.speed_y * elapsed;
 	this.lastStep = now;
 
+	this.checkCollisions ();
+
 	if ((this.area != null) && ((now - this.lastSendMsg) > this.sendMsgInterval)) {
 		this.sendUpdateMsg();
+	}
+}
+
+ObjectUpdater.prototype.checkCollisions = function () {
+	var self = this;
+	this.collisionChecks.forEach (function (record) {
+		if (self.collide (self.object, record.other)) {
+			record.callback (self.object, record.other);
+		}
+	});
+}
+
+ObjectUpdater.prototype.collide = function (one, another) {
+	var min_distance = one.radius + another.radius;
+	if (
+		(Math.abs (one.x - another.x) < min_distance)
+		&& (Math.abs (one.y - another.y) < min_distance)
+		&& (Math.sqrt (Math.pow (one.x - another.x, 2) + Math.pow (one.y - another.y, 2)) < min_distance)
+	) {
+		return true;
+	} else {
+		return false;
 	}
 }
 
@@ -35,4 +60,20 @@ ObjectUpdater.prototype.sendUpdateMsg = function () {
 
 ObjectUpdater.prototype.setAreaToSendUpdates = function (area) {
 	this.area = area;
+}
+
+ObjectUpdater.prototype.addCollisionCheck = function (other, callback) {
+	this.collisionChecks.push ({
+		other: other,
+		callback: callback
+	});
+}
+
+ObjectUpdater.prototype.removeCollisionCheck = function (other) {
+	//FIXME: esto deberia estar sincronizado como seccion critica
+	for (var i=0; i<this.collisionChecks.length; i++) {
+		if (this.collisionChecks[i].other.id == other.id) {
+			this.collisionChecks.splice(i, 1);
+		}
+	}
 }
